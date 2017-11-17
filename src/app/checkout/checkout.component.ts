6525302de13250867310e4f8d9af4ec40a8d7819
Dinuksha Ishwari <dinukshai@zone24x7.com>
2017-11-17 07:45:52 +0530

    Add checkout functionality

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CheckinService } from '../checkin/checkin.service';
+import swal from 'sweetalert2';
+import { Trip } from '../models/trip.model';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  trip: Trip = new Trip();
+  
+  constructor(private _router: Router, private _checkinService: CheckinService) { }
 
   ngOnInit() {
+  }
+
+  validateToken() {
+    this._checkinService.validateToken(this.trip.tokenRef).subscribe(data => {
+      console.log(data._body);
+      if (data._body === "true") {
+        swal({
+          title: 'Success!',
+          text: 'Your token has been verfied! ',
+          type: 'success',
+          confirmButtonText: 'OK',
+        }).then(() => {
+          this._checkinService.checkout(this.trip).subscribe( data => {
+            this._router.navigateByUrl('/home');
+            location.reload();
+          })
+        });
+      }
+      else {
+        swal({
+          title: 'Error!',
+          text: 'Failed to verify token! ',
+          type: 'error',
+          confirmButtonText: 'OK',
+        }).then(() => {
+
+          // Add pay with cash option
+
+        });
+      }
+    });
   }
 
 }
